4d4a5c34300e4dbfd74650defb1faedce1b51a3b
Ingosi <bingosi@leisdstudent.ws>
2022-03-22 14:51:05 -0500

    second commit, have the name question correct, still working on asking conversion factor and what temperature number they'd like to convert from

---
+++ app.js
@@ -0,0 +1,17 @@
+function askname() {
+    var name = prompt("What is your name?")
+    return alert(`Hello ${name}`)
+}
+
+alert(askname())
+
+function convert() {
+    var conv = prompt("Would you like to convert from `c` or `f`")
+
+}
+
+function temperature() {
+    var temp = prompt("Enter a temperature")
+}
+
+alert(temperature())
