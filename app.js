5a5b201dba3d4c0e7148695b7a24ecbb38b493e2
Ingosi <bingosi@leisdstudent.ws>
2022-03-23 14:54:21 -0500

    3rd commit everything is added but need to fix

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 function askname() {
     var name = prompt("What is your name?")
-    return alert(`Hello ${name}`)
+    return alert(`Hello, ${name}`)
 }
 
 alert(askname())
 
 function convert() {
     var conv = prompt("Would you like to convert from `c` or `f`")
-
+    return alert("Ok.")
 }
 
-function temperature() {
-    var temp = prompt("Enter a temperature")
+alert(convert())
+
+function enter(a) {
+    var entert = prompt("Ok what temperature would you like to convert from?")
+    return alert(`Ok, ${entert} degrees is...`)
 }
 
-alert(temperature())
+alert (enter())
+
+function ctof() {
+    entert - 32 * 5 / 9
+}
+
+
+function ftoc() {
+    entert * 9 / 5 + 32
+}
+
+
